5148c38b878f73890fd73878eda38f16d58bd62d
Polina Tiurina <polina.tiurina@kartenmacherei.de>
2022-05-22 17:58:41 +0200

    add doctor entity and migration for doctor table

---
+++ migrations/Version20220522145237.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20220522145237 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create /"doctor"/ table"';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE doctor (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE doctor');
+    }
+}
